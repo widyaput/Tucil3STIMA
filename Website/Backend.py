39efc769e6b2670eebad5593e77f30cf3854eb3e
Muhammad Fahmi Alamsyah <49779495+tararahuuw@users.noreply.github.com>
2021-04-03 20:58:32 +0700

    checkbox

--- Website/Backend.py
+++ Website/Backend.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import folium
 from branca.element import Figure
 
@@ -17,9 +17,28 @@ def index():
     m5.save('templates/map.html')
     return render_template('GoogleMaps.html')
 
+@app.route('/', methods = ['POST'])
+def getvalue():
+    firstloc = request.form['floc']
+    endloc = request.form['eloc']
+    return render_template('GoogleMaps.html', firstloc=firstloc, endloc=endloc)
+
+    
+@app.route('/', methods = ['POST'])
+def getform():
+    kota2 = request.form['city']
+    return render_template('GoogleMaps.html', firstloc=firstloc, endloc=endloc)
+    
 @app.route('/map')
 def map():
     return render_template('map.html')
+
+@app.route('/register' , methods=['POST'])
+def test():
+    firstloc = request.form['floc']
+    endloc = request.form['eloc']
+    learning_style = request.form.get("comp_select")
+    return render_template('GoogleMaps.html', kota = learning_style, firstloc = firstloc, endloc= endloc)
 
 if __name__ == '__main__':
     app.run(debug=True)
